04a7aa907a8d61a4cf1326a1a457e46fccbdfa4a
Davi <104468458+daviijose@users.noreply.github.com>
2022-09-08 19:53:21 -0300

    att

---
+++ src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+
+ export default function Header(){
+    return (
+        <div className="logo">
+           <h1>Velg </h1> 
+        </div> 
+    );
+
+ }
